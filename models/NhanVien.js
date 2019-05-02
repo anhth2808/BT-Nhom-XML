4acd3066a72c617a05a8266fc94074ab56c7197e
TranHoangAnh <lockedheart98@outlook.com>
2019-05-02 22:35:27 +0700

    upadate

--- models/NhanVien.js
+++ models/NhanVien.js
@@ -94,107 +94,95 @@ class NhanVien {
     
     save() {
         return new Promise((resolve, reject) => {
-            getDataFromFile(filec => {
-                if (this.MaNV) { // edit nhanvien
-              
-                    getDocument(doc => {
-                        const eNhanVien = doc.getElementsByTagName("NhanVien");
-
-                        
-                        for (let i = 0; i < eNhanVien.length; i++) {
-                            if (eNhanVien[i].getElementsByTagName("MaNV")[0].childNodes[0].nodeValue === this.MaNV) {
-                                eNhanVien[i].getElementsByTagName("TenNV")[0].childNodes[0].textContent = this.TenNV;
-                                eNhanVien[i].getElementsByTagName("DiaChi")[0].childNodes[0].textContent = this.DiaChi; 
-                                eNhanVien[i].getElementsByTagName("NgaySinh")[0].childNodes[0].textContent = this.NgaySinh;
-                                eNhanVien[i].getElementsByTagName("GioiTinh")[0].childNodes[0].textContent = this.GioiTinh;
-                                eNhanVien[i].getElementsByTagName("DanToc")[0].childNodes[0].textContent = this.DanToc;
-                                eNhanVien[i].getElementsByTagName("TonGiao")[0].childNodes[0].textContent = this.TonGiao;
-                                eNhanVien[i].getElementsByTagName("CMND")[0].childNodes[0].textContent = this.CMND;
-                                eNhanVien[i].getElementsByTagName("MaPB")[0].childNodes[0].textContent = this.MaPB;
-                                eNhanVien[i].getElementsByTagName("MaCV")[0].childNodes[0].textContent = this.MaCV;
-                            }
-                        }
-                        const xmlData = serializer.serializeToString(doc);
-                        
-                        fs.writeFile(p, xmlData, "utf-8", () => {
-                            console.log(this);
-                            resolve(this);
-                        });
-
-                    });                  
-
-                } else { // add new nhanvien
-                    getDocument((doc) => {
-
-                        this.MaNV = Math.random().toString();
-                        this.MaHDLD = Math.random().toString();
-                        // nhanViens.push(this);
-
-
-                        const eNhanVien = doc.createElement("NhanVien");
-                        
-                        const eMaNV = doc.createElement("MaNV");
-                        eMaNV.textContent = this.MaNV;
-
-                        const eTenNV = doc.createElement("TenNV");
-                        eTenNV.textContent = this.TenNV;
-
-                        const eDiaChi = doc.createElement("DiaChi");
-                        eDiaChi.textContent = this.DiaChi;
-
-                        const eNgaySinh = doc.createElement("NgaySinh");
-                        eNgaySinh.textContent = this.NgaySinh;
-
-                        const eGioiTinh = doc.createElement("GioiTinh");
-                        eGioiTinh.textContent = this.GioiTinh;
-
-                        const eDanToc = doc.createElement("DanToc");
-                        eDanToc.textContent = this.DanToc;
-
-                        const eTonGiao = doc.createElement("TonGiao");                     
-                        eTonGiao.textContent = this.TonGiao;
-
-                        const eCMND = doc.createElement("CMND");
-                        eCMND.textContent = this.CMND;
-                        
-
-                        const eMaPB = doc.createElement("MaPB");
-                        eMaPB.textContent = this.MaPB;
-
-                        const eMaCV = doc.createElement("MaCV");
-                        eMaCV.textContent = this.MaCV;
-
-                        const eMaHDLD = doc.createElement("MaHDLD");
-                        eMaHDLD.textContent = this.MaHDLD;
-
-
-                        eNhanVien.appendChild(eMaNV);
-                        eNhanVien.appendChild(eTenNV);
-                        eNhanVien.appendChild(eDiaChi);
-                        eNhanVien.appendChild(eNgaySinh);
-                        eNhanVien.appendChild(eGioiTinh);
-                        eNhanVien.appendChild(eDanToc);
-                        eNhanVien.appendChild(eTonGiao);
-                        eNhanVien.appendChild(eCMND);
-                        eNhanVien.appendChild(eMaPB);
-                        eNhanVien.appendChild(eMaCV);
-                        eNhanVien.appendChild(eMaHDLD);
-
-                        const eQuanLyNhanVien = doc.getElementsByTagName("QuanLyNhanVien");
-                        eQuanLyNhanVien[0].appendChild(eNhanVien);
-
-                        // console.log(eQuanLyNhanVien[0]);
-                        const xmlData = serializer.serializeToString(doc);
-                        
-                        fs.writeFile(p, xmlData, "utf-8", () => {
-                            resolve(this);
-                        });
-                        // wirteFile
+            if (this.MaNV) { // edit nhanvien
+                getDocument(doc => {
+                    const eNhanVien = doc.getElementsByTagName("NhanVien");
                     
-                    })
-                }
-            });
-        })
+                    for (let i = 0; i < eNhanVien.length; i++) {
+                        if (eNhanVien[i].getElementsByTagName("MaNV")[0].childNodes[0].nodeValue === this.MaNV) {
+                            eNhanVien[i].getElementsByTagName("TenNV")[0].childNodes[0].textContent = this.TenNV;
+                            eNhanVien[i].getElementsByTagName("DiaChi")[0].childNodes[0].textContent = this.DiaChi; 
+                            eNhanVien[i].getElementsByTagName("NgaySinh")[0].childNodes[0].textContent = this.NgaySinh;
+                            eNhanVien[i].getElementsByTagName("GioiTinh")[0].childNodes[0].textContent = this.GioiTinh;
+                            eNhanVien[i].getElementsByTagName("DanToc")[0].childNodes[0].textContent = this.DanToc;
+                            eNhanVien[i].getElementsByTagName("TonGiao")[0].childNodes[0].textContent = this.TonGiao;
+                            eNhanVien[i].getElementsByTagName("CMND")[0].childNodes[0].textContent = this.CMND;
+                            eNhanVien[i].getElementsByTagName("MaPB")[0].childNodes[0].textContent = this.MaPB;
+                            eNhanVien[i].getElementsByTagName("MaCV")[0].childNodes[0].textContent = this.MaCV;
+                        }
+                    }
+                    const xmlData = serializer.serializeToString(doc);
+                    
+                    fs.writeFile(p, xmlData, "utf-8", () => {
+                        console.log(this);
+                        resolve(this);
+                    });
+
+                });                  
+
+            } else { // add new nhanvien
+                getDocument((doc) => {
+                    this.MaNV = Math.random().toString();
+                    this.MaHDLD = Math.random().toString();
+
+                    const eNhanVien = doc.createElement("NhanVien");
+                    
+                    const eMaNV = doc.createElement("MaNV");
+                    eMaNV.textContent = this.MaNV;
+
+                    const eTenNV = doc.createElement("TenNV");
+                    eTenNV.textContent = this.TenNV;
+
+                    const eDiaChi = doc.createElement("DiaChi");
+                    eDiaChi.textContent = this.DiaChi;
+
+                    const eNgaySinh = doc.createElement("NgaySinh");
+                    eNgaySinh.textContent = this.NgaySinh;
+
+                    const eGioiTinh = doc.createElement("GioiTinh");
+                    eGioiTinh.textContent = this.GioiTinh;
+
+                    const eDanToc = doc.createElement("DanToc");
+                    eDanToc.textContent = this.DanToc;
+
+                    const eTonGiao = doc.createElement("TonGiao");                     
+                    eTonGiao.textContent = this.TonGiao;
+
+                    const eCMND = doc.createElement("CMND");
+                    eCMND.textContent = this.CMND;
+                    
+                    const eMaPB = doc.createElement("MaPB");
+                    eMaPB.textContent = this.MaPB;
+
+                    const eMaCV = doc.createElement("MaCV");
+                    eMaCV.textContent = this.MaCV;
+
+                    const eMaHDLD = doc.createElement("MaHDLD");
+                    eMaHDLD.textContent = this.MaHDLD;
+
+
+                    eNhanVien.appendChild(eMaNV);
+                    eNhanVien.appendChild(eTenNV);
+                    eNhanVien.appendChild(eDiaChi);
+                    eNhanVien.appendChild(eNgaySinh);
+                    eNhanVien.appendChild(eGioiTinh);
+                    eNhanVien.appendChild(eDanToc);
+                    eNhanVien.appendChild(eTonGiao);
+                    eNhanVien.appendChild(eCMND);
+                    eNhanVien.appendChild(eMaPB);
+                    eNhanVien.appendChild(eMaCV);
+                    eNhanVien.appendChild(eMaHDLD);
+
+                    const eQuanLyNhanVien = doc.getElementsByTagName("QuanLyNhanVien");
+                    eQuanLyNhanVien[0].appendChild(eNhanVien);
+
+                    const xmlData = serializer.serializeToString(doc);
+                    fs.writeFile(p, xmlData, "utf-8", () => {
+                        resolve(this);
+                    });
+                });
+            }
+        });
     }
 
     static fetchAll(cb) {
@@ -212,9 +200,8 @@ class NhanVien {
         getDocument(doc => {
             const eNhanVien = doc.getElementsByTagName("NhanVien");
 
-
-
-            let MaHDLD; // ma hdld dung de delete;
+            // ma hdld dung de delete;
+            let MaHDLD; 
 
             for (let i = 0; i < eNhanVien.length; i++) {
                 if (eNhanVien[i].getElementsByTagName("MaNV")[0].childNodes[0].nodeValue === MaNV) {                    
@@ -224,14 +211,8 @@ class NhanVien {
                     deletedElement.parentNode.removeChild(deletedElement);
                 }
             }
-
-            // const temp = doc.getElementsByTagName("NhanVien");
-            // for (let i = 0; i < temp.length; i++) {                 
-            //     console.log(temp[i].getElementsByTagName("MaNV")[0].childNodes[0].nodeValue) 
-            //     // deletedElement.parentNode.removeChild(deletedElement);
-            // }
-
-            
+        
+            //
             HDLD.deleteById(MaHDLD);
 
             const xmlData = serializer.serializeToString(doc);           
